06811d224f1e0bfaeb20de72ff1e6f788b607983
yuki <notsmile@proton.me>
2022-11-05 13:36:04 -0400

    Initial Commit

--- src/main/java/code/yuki/Main.java
+++ src/main/java/code/yuki/Main.java
@@ -1,12 +1,13 @@
 package code.yuki;
 
-import code.yuki.commands.builder.CommandBuilder;
+import code.yuki.commands.builder.CMDCommandBuilder;
+import code.yuki.commands.builder.util.CommandBuilderUtil;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        CommandBuilder builder = new CommandBuilder();
-        String cmd = builder.addPrefix("netsh").netshInterface().customString(" 6to4 set state").valueSetting("disabled").completeCommand();
+        CMDCommandBuilder builder = new CMDCommandBuilder();
+        String cmd = builder.addPrefix("netsh").netshInterface().customString(" 6to4 set state ").valueSetting("disabled").builderToString(builder);
         System.out.println(cmd);
     }
 }
