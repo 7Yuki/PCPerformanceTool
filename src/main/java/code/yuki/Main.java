e9a9a9bf5ff34a1766b566bfde6ead05fad9ca3b
yuki <notsmile@proton.me>
2022-11-09 11:34:33 -0500

    Initial Commit

--- src/main/java/code/yuki/Main.java
+++ src/main/java/code/yuki/Main.java
@@ -7,7 +7,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         CMDCommandBuilder builder = new CMDCommandBuilder();
-        String cmd = builder.addPrefix("netsh").netshInterface().customString(" 6to4 set state ").valueSetting("disabled").builderToString(builder);
+        String cmd = builder
+                .addPrefix("netsh")
+                .netshInterface()
+                .customString(" 6to4 set state ")
+                .valueSetting("disabled")
+                .builderToString(builder);
         System.out.println(cmd);
     }
 }
